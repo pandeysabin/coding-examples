3a82f6e6a62ba62251fb6410fecdc68700ad6c84
Sabin Pandey <pandeyfcb7849@gmail.com>
2021-01-24 20:38:07 +0545

    New problem solved for today.

---
+++ console-input-2021-1-24_8-10-53.js
@@ -0,0 +1,29 @@
+// Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+// After doing so, return the array.
+
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+
+const replaceElements = function (arr) {
+  const length = arr.length;
+  let largestNoToTheRight = arr[length - 1];
+
+  for (let index = length - 1; index >= 0; index--) {
+    if (index === length - 1) {
+      arr[index] = -1;
+    } else if (largestNoToTheRight < arr[index]) {
+      arr[index] = arr[index] - largestNoToTheRight;
+      largestNoToTheRight = arr[index] + largestNoToTheRight;
+      arr[index] = largestNoToTheRight - arr[index];
+    } else {
+      arr[index] = largestNoToTheRight;
+    }
+  }
+
+  return arr;
+};
+
+replaceElements([17, 18, 5, 4, 6, 1]);
