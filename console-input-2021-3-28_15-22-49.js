b7cb07eb051d27c5e252986e27b64d10748b35eb
Sabin Pandey <pandeyfcb7849@gmail.com>
2021-04-03 13:26:55 +0545

    New programming challenge.

---
+++ console-input-2021-3-28_15-22-49.js
@@ -0,0 +1,31 @@
+// Students are asked to stand in non-decreasing order of heights for an annual photo.
+
+// Return the minimum number of students that must move in order for all students to be standing in non-decreasing order of height.
+
+// Notice that when a group of students is selected they can reorder in any possible way between themselves and the non selected students remain on their seats.
+
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+
+const anotherHeightChecker = function (heights) {
+  let newOrderHeights = [...heights];
+
+  newOrderHeights.sort((a, b) => a - b);
+
+  const numberOfOrderChanges = newOrderHeights.reduce(
+    (acc, curr, currentIndex) => {
+      if (curr === heights[currentIndex]) {
+        return acc;
+      }
+      const newNumberOfOrdersChanges = acc + 1;
+      return newNumberOfOrdersChanges;
+    },
+    0
+  );
+
+  return numberOfOrderChanges;
+};
+
+console.log(anotherHeightChecker());
