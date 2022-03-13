9ac9ad494f1cff6cbb3007a5bafeb8ac17a221db
Sabin Pandey <sabinpandey@Sabins-MacBook-Air.local>
2022-03-13 14:35:32 +0545

    magical cows problem

--- console-input-2022-2-6_16-5-26.ts
+++ console-input-2022-2-6_16-5-26.ts
@@ -10,30 +10,39 @@ const magicalCowProblem = ({
   maxNoOfCows,
   cowsInEachFarm,
   numberOfDifferentDaysRegulatorVisiting,
-}: MagicalCowProblemParams) => {
+}: MagicalCowProblemParams): number[] => {
   let eachDay = 0;
 
   let updatedFarmsWithCow = [...cowsInEachFarm];
 
+  let numberOfFarmsEachDay = [];
+
   do {
     if (eachDay !== 0) {
-      let farmsWithDoubledCow = cowsInEachFarm.map((noOfCows) => {
+      [...updatedFarmsWithCow].forEach((noOfCows, index) => {
         const numberOfCows = noOfCows * 2;
 
         if (numberOfCows > maxNoOfCows) {
+          const numberOfCowsAfterHalf = numberOfCows / 2;
+          updatedFarmsWithCow[index] = numberOfCowsAfterHalf;
+          updatedFarmsWithCow.push(numberOfCowsAfterHalf);
+        } else {
+          updatedFarmsWithCow[index] = numberOfCows;
         }
       });
     }
+    numberOfFarmsEachDay.push(updatedFarmsWithCow.length);
+
     eachDay++;
   } while (eachDay < numberOfDifferentDaysRegulatorVisiting);
 
-  return updatedFarmsWithCow;
+  return numberOfFarmsEachDay;
 };
 
 const params = {
   maxNoOfCows: 2,
-  cowsInEachFarm: [1, 2, 1, 2, 2],
-  numberOfDifferentDaysRegulatorVisiting: 0,
+  cowsInEachFarm: [1, 2, 1, 2, 1],
+  numberOfDifferentDaysRegulatorVisiting: 3,
 };
 
 const newFarmsWithCow = magicalCowProblem(params);
