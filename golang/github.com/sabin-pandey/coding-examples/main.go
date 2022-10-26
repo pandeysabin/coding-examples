394149bbcc2e6ef6b04c280910c09fa5033e73c0
Sabin Pandey <pandeyfcb7849@gmail.com>
2022-10-26 17:35:23 +0545

    Add new linked-list solution with typescript & go lang

--- golang/github.com/sabin-pandey/coding-examples/main.go
+++ golang/github.com/sabin-pandey/coding-examples/main.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 
 	"github.com/sabin-pandey/coding-examples/intersection_of_two_linked_lists"
+
+	"github.com/sabin-pandey/coding-examples/remove_nth_node_from_end_of_list"
 )
 
 func main() {
 	fmt.Println("sabin")
 
 	intersection_of_two_linked_lists.GetIntersectionNode()
+
+	remove_nth_node_from_end_of_list.RemoveNthFromEnd()
 }
